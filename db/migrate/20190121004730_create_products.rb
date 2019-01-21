9dbeddecc4456c48feeffcb124fb469b87e354e8
Meder <m.toktosunov@icloud.com>
2019-01-20 20:06:26 -0500

    created db and migrated

--- db/migrate/20190121004730_create_products.rb
+++ db/migrate/20190121004730_create_products.rb
@@ -1,8 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string, :title
-      t.float, :price
+      t.string :title
+      t.float :price
       t.integer :inventory_count
 
       t.timestamps
